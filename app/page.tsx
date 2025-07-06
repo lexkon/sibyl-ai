c273a53461d43d3ea38f4af3483795af1498dc96
Alex Kontis <alex@alexkontis.co.uk>
2025-07-06 16:21:15 +0100

    Home page subheading

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import FileSelector from "@/components/FileSelector"
 export default function Home() {
   return (
     <main className="text-center">
-      <h1 className="p-8 text-6xl font-heading text-zinc-800 dark:text-zinc-300">Sibyl</h1>
+      <h1 className="pt-8 mb-4 text-6xl font-heading text-zinc-800 dark:text-zinc-300">Sibyl</h1>
+      <h1 className="mb-8 text-3xl font-heading text-zinc-700 dark:text-zinc-300">Generate podcast titles and description</h1>
       <FileSelector />
     </main>
   )
